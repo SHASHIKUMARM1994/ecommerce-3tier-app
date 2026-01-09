e5d81ec99bd1215e4cd93fddfe0788b0d40cb666
SHASHIKUMARM <128211901+SHASHIKUMARM1994@users.noreply.github.com>
2026-01-09 22:45:09 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+app.get('/', (req, res) => {
+  res.send('E-Commerce Backend Running');
+});
+
+app.get('/products', (req, res) => {
+  db.query('SELECT * FROM products', (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+app.listen(3000, () => console.log('App running on port 3000'));
 
